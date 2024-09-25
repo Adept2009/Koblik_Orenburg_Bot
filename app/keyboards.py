c1434859d6d189f288da1d64c8a48a79d90a6ef2
Adept2009 <gnomy2003@mail.ru>
2024-09-25 11:20:16 +0500

    Рабочая версия. Отправка в группу и на e-mail

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,8 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-
 main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Near-miss')],
-                                     [KeyboardButton(text='О боте')],
-                                     [KeyboardButton(text='О нас')]],
-                           resize_keyboard=True,
-                           input_field_placeholder='Выберите пункт меню...')
+                                     [KeyboardButton(text='О боте'),
+                                      KeyboardButton(text='О нас')]],
+                           resize_keyboard=True, one_time_keyboard=False,
+                           input_field_placeholder='Выберите пункт меню...')  # Параметр resize_keyboard - отвечает
+                                        # за более мелкий размер кнопок (True), параметр one_time_keyboard - отвечает
+                                        # за то чтобы кнопки не пропадали, а сворачивались вниз (False)
